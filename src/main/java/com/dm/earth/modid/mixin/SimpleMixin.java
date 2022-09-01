a284d68c4599afe537fb790ffa4bbd80be2d128c
Yjn024 <jiening.yu@outlook.com>
2022-09-01 21:14:04 +0800

    dm tm

--- src/main/java/com/dm/earth/modid/mixin/SimpleMixin.java
+++ src/main/java/com/dm/earth/modid/mixin/SimpleMixin.java
@@ -1,6 +1,5 @@
-package net.fabricmc.example.mixin;
+package com.dm.earth.modid.mixin;
 
-import net.fabricmc.example.ExampleMod;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -8,9 +7,8 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
-public class ExampleMixin {
+public class SimpleMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
